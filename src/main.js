e050a3fb7e9488b007ab354eb0c14fb0a31ce2be
antonio_amado <ac.marcel@gmail.com>
2017-05-07 23:27:27 -0500

    finish first app demo

--- src/main.js
+++ src/main.js
@@ -1,20 +1,45 @@
 const electron = require('electron')
+const countdown = require('./countdown')
 
 const app = electron.app
+const ipc = electron.ipcMain
 const {
   BrowserWindow,
 } = electron
 
-app.on('ready', (_) => {
-	var mainWindow = new BrowserWindow({
-		height: 400,
-		width: 400,
+const windows = []
+app.on('ready', () => {
+	[1, 2, 3].forEach(i => {
+		let params = {
+			height: 400,
+			width: 400
+		}
+		switch (i) {
+		case 1:
+			params.frame = false
+			break
+		case 2:
+			params.titleBarStyle = 'hidden'
+			break
+		default:
+			params.titleBarStyle = 'hidden-inset'
+		}
+		let win = new BrowserWindow(params)
+
+		win.loadURL(`file://${__dirname}/countdown.html`)
+
+		win.on('closed', () => {
+			win = null
+		})
+
+		windows.push(win)
 	})
+})
 
-	mainWindow.loadURL(`file://${__dirname}/countdown.html`)
-
-	mainWindow.on('closed', (_) => {
-		console.log('closed')
-		mainWindow = null
+ipc.on('countdown-start', () => {
+	windows.forEach(win => {
+		countdown(count => {
+			win.webContents.send('countdown', count)
+		})
 	})
 })
